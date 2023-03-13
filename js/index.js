f5762a7a9803e7a275f6046fad75e15d1300cda9
Max Santana <iimaxsany@gmail.com>
2023-03-13 11:48:11 -0600

    Version 0.1 Se cambian variables para hacer enteendible más legible el codigo

--- js/index.js
+++ js/index.js
@@ -1,27 +1,26 @@
-let walls = [0, 1, 0, 2, 1, 0, 3, 2, 1, 1, 1, 5, 0, 1];
-let maxHeights = [];
+let heights = [0, 1, 0, 2, 1, 0, 3, 2, 1, 1, 1, 5, 0, 1];
+let maxHeightWalls = [];
 let snowHeight;
 let max = 0;
 let snow = 0;
 
 function SnowMeter() {
-    console.log(walls);
-    walls.forEach(function(wallHeight, i) {
-        if (wallHeight > max && wallHeight > walls[i + 1]) {
+    heights.forEach(function(wallHeight, i) {
+        if (wallHeight > max && wallHeight > heights[i + 1]) {
             max = wallHeight;
-            maxHeights.push(i);
+            maxHeightWalls.push(i);
         }
     });
     max = 0;
     let maxHeightsAux = [];
-    for (var i = walls.length; i > 0; i--) {
-        if (walls[i] > max && walls[i] > walls[i - 1]) {
-            max = walls[i];
+    for (var i = heights.length; i > 0; i--) {
+        if (heights[i] > max && heights[i] > heights[i - 1]) {
+            max = heights[i];
             maxHeightsAux.unshift(i);
         }
     }
-    maxHeights = maxHeights.concat(maxHeightsAux);
-    console.log(maxHeights);
+    maxHeightWalls = maxHeightWalls.concat(maxHeightsAux);
+
 
 
 }
